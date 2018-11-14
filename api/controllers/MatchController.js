62da358470fa3f3d1918f01d2e04678fd4b5ca19
Juan Manuel Sanchez <sanchez.juanmy@gmail.com>
2018-11-13 22:02:52 -0300

    populate palyers in match.goals

--- api/controllers/MatchController.js
+++ api/controllers/MatchController.js
@@ -6,7 +6,17 @@
  */
 
 module.exports = {
-  
 
+    findOne: async function (req, res) {
+        let id = req.param('id');
+        let match = await Match.findOne({ id: id }).populate('goals1').populate('goals2').populate('team1').populate('team2')
+        for (const goal of match.goals1) {
+            goal.player = await Player.findOne({ id: goal.player });
+        }
+        for (const goal of match.goals2) {
+            goal.player = await Player.findOne({ id: goal.player });
+        }
+        return res.json(match);
+    }
 };
 
